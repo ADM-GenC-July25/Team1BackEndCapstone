21447565fd1d86fd7068917adb1ae3a878a9c2b0
eperry910 <eperry910@yahoo.com>
2025-08-28 13:47:11 -0400

    Added AWS front-end to CORS policy

--- src/main/java/com/example/bytebazaar/config/SecurityConfig.java
+++ src/main/java/com/example/bytebazaar/config/SecurityConfig.java
@@ -41,7 +41,8 @@ public class SecurityConfig {
             "http://127.0.0.1:3000",
             "http://127.0.0.1:4200",
             "http://127.0.0.1:8080",
-            "http://127.0.0.1:5173"
+            "http://127.0.0.1:5173",
+            "http://978358-capstone-team1.s3-website-us-west-2.amazonaws.com"
             // Temporarily allow all origins for development (REMOVE IN PRODUCTION!)
             // "*"
         ));
